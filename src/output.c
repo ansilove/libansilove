9bd2b0166f5cdfb524aaba856bbe36f3afe46600
Frederic Cambus <fred@statdns.com>
2018-10-24 14:52:31 +0200

    Create DOS aspect ratio in a single pass, this makes things faster, uses less RAM, and output is similar

--- src/output.c
+++ src/output.c
@@ -20,24 +20,15 @@ int output(struct ansilove_ctx *ctx, struct ansilove_options *options, gdImagePt
 	if (!options->retinaScaleFactor && !options->dos) {
 		ctx->png.buffer = gdImagePngPtr(im_Source, &ctx->png.length);
 	} else if (options->dos) {
-		gdImagePtr im_Resize;
 		gdImagePtr im_DOS;
 
-		im_Resize = gdImageCreate(im_Source->sx,
-		    im_Source->sy * 27);
+		im_DOS = gdImageCreateTrueColor(im_Source->sx,
+		    im_Source->sy * 1.35);
 
-		gdImageCopyResized(im_Resize, im_Source, 0, 0, 0, 0,
-		    im_Resize->sx, im_Resize->sy, im_Source->sx, im_Source->sy);
+		gdImageCopyResampled(im_DOS, im_Source, 0, 0, 0, 0,
+		    im_DOS->sx, im_DOS->sy, im_Source->sx, im_Source->sy);
 
 		gdImageDestroy(im_Source);
-
-		im_DOS = gdImageCreateTrueColor(im_Resize->sx,
-		    im_Resize->sy / 20);
-
-		gdImageCopyResampled(im_DOS, im_Resize, 0, 0, 0, 0,
-		    im_DOS->sx, im_DOS->sy, im_Resize->sx, im_Resize->sy);
-
-		gdImageDestroy(im_Resize);
 
 		ctx->png.buffer = gdImagePngPtr(im_DOS, &ctx->png.length);
 	} else {
