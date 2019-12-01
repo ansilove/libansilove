44c20814c1c5d4fc59c86c319f99a51216fc8466
Ahmed Yasin KUL <5943105+AHOME@users.noreply.github.com>
2019-12-01 16:27:22 +0300

    Example file has been improved.
    example.c file has been improved by using dynamically allocated structs, which allows to send parameters to other functions.

--- example/example.c
+++ example/example.c
@@ -1,16 +1,32 @@
+#include <stdio.h>
+#include <errno.h>
 #include <ansilove.h>
 
 int main() {
-	struct ansilove_ctx ctx;
-	struct ansilove_options options;
+	struct ansilove_ctx* ctx = malloc(sizeof *ctx);
+	struct ansilove_options* options = malloc(sizeof *options);
+	
+	if (ctx && options) {
 
-	ansilove_init(&ctx, &options);
+		ansilove_init(ctx, options);
 
-	ansilove_loadfile(&ctx, "example.c");
+		ansilove_loadfile(ctx, "example.c");
 
-	ansilove_ansi(&ctx, &options);
+		ansilove_ansi(ctx, options);
 
-	ansilove_savefile(&ctx, "example.png");
+		ansilove_savefile(ctx, "example.png");
 
-	ansilove_clean(&ctx);
+		ansilove_clean(ctx);
+
+		free(ctx);
+		free(options);
+
+	}
+
+	else {
+		printf("Malloc function is failed...\n");
+		return -ENOMEM;
+	}
+	
+	return 0;
 }
