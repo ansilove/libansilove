7d417b8d7e5cd8fe364be017e3d32d9b58e0fd47
Frederic Cambus <fred@statdns.com>
2020-10-22 10:37:50 +0200

    Add LibFuzzer-based fuzzers for each supported format.

---
+++ fuzz/tundra.c
@@ -0,0 +1,18 @@
+#include <ansilove.h>
+
+int
+LLVMFuzzerTestOneInput(uint8_t *data, size_t size)
+{
+	struct ansilove_ctx ctx;
+	struct ansilove_options options;
+
+	ansilove_init(&ctx, &options);
+
+	ctx.buffer = data;
+	ctx.length = size;
+
+	ansilove_tundra(&ctx, &options);
+
+	ansilove_clean(&ctx);
+	return 0;
+}
