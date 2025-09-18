0308363e77734c364e1eaa238e7134ad7f1f4142
Tom Aylott <oblivious@subtlegradient.com>
2025-09-18 19:03:59 -0400

    Add flake.nix for Nix development environment
    Introduces a flake.nix file to manage the development environment for libansilove using Nix Flakes. This includes defining packages and a
    development shell with necessary build tools and libraries.

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Development environment for libansilove";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        version = "1.4.2";
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "libansilove";
+          inherit version;
+          src = ./.;
+          nativeBuildInputs = [
+            pkgs.cmake
+            pkgs.pkg-config
+          ];
+          buildInputs = [
+            pkgs.libgd
+          ];
+          cmakeFlags = [
+            "-DCMAKE_BUILD_TYPE=Release"
+          ];
+          meta = with pkgs.lib; {
+            description = "Library to convert ANSI and artscene formats to PNG";
+            homepage = "https://www.ansilove.org";
+            license = licenses.bsd2;
+            maintainers = [];
+            platforms = platforms.unix;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.cmake
+            pkgs.ninja
+            pkgs.pkg-config
+            pkgs.libgd
+            pkgs.clang
+            pkgs.clang-tools
+            pkgs.gdb
+          ];
+          shellHook = ''
+            export CC=${pkgs.clang}/bin/clang
+            export CXX=${pkgs.clang}/bin/clang++
+            echo "libansilove dev shell loaded."
+          '';
+        };
+      }
+    );
+}
