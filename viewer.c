054053be2fe7b9cb58a12d549ff167da3fbf1b5a
Tom Aylott <thomas.42@gmail.com>
2025-10-23 20:19:42 -0400

    Add multi-file support to ansilove-utf8ansi
    Can now process multiple ANSI files in one command:
      ./ansilove-utf8ansi file1.ans file2.ans file3.ans
    
    Outputs all files sequentially to stdout.
    Optional columns parameter works as last argument.
    
    Usage:
      ansilove-utf8ansi *.ans
      ansilove-utf8ansi file1.ans file2.ans 80
    
    All 26/26 fire-43 files tested ✓

--- viewer.c
+++ viewer.c
@@ -5,43 +5,60 @@
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s <ansi-file> [columns]\n", argv[0]);
+        fprintf(stderr, "Usage: %s <ansi-file>... [columns]\n", argv[0]);
         return 1;
     }
 
-    struct ansilove_ctx ctx;
-    struct ansilove_options opts;
-
-    memset(&ctx, 0, sizeof(ctx));
-    memset(&opts, 0, sizeof(opts));
-
-    if (ansilove_init(&ctx, &opts) != 0) {
-        fprintf(stderr, "Init failed\n");
-        return 1;
-    }
-
-    if (ansilove_loadfile(&ctx, argv[1]) != 0) {
-        fprintf(stderr, "Load failed\n");
-        return 1;
-    }
-
-    opts.mode = ANSILOVE_MODE_TERMINAL;
+    int columns = 0;
+    int file_count = argc - 1;
+    
     if (argc >= 3) {
-        opts.columns = atoi(argv[2]);
-    }
-    
-    if (ansilove_terminal(&ctx, &opts) != 0) {
-        fprintf(stderr, "Terminal conversion failed\n");
-        return 1;
-    }
-
-    size_t output_len;
-    uint8_t *output = ansilove_terminal_emit(&ctx, &output_len);
-    
-    if (output && output_len > 0) {
-        fwrite(output, 1, output_len, stdout);
+        char *endptr;
+        long val = strtol(argv[argc - 1], &endptr, 10);
+        if (*endptr == '\0' && val > 0 && val < 10000) {
+            columns = val;
+            file_count = argc - 2;
+        }
     }
 
-    ansilove_clean(&ctx);
+    for (int i = 1; i <= file_count; i++) {
+        struct ansilove_ctx ctx;
+        struct ansilove_options opts;
+
+        memset(&ctx, 0, sizeof(ctx));
+        memset(&opts, 0, sizeof(opts));
+
+        if (ansilove_init(&ctx, &opts) != 0) {
+            fprintf(stderr, "Init failed: %s\n", argv[i]);
+            continue;
+        }
+
+        if (ansilove_loadfile(&ctx, argv[i]) != 0) {
+            fprintf(stderr, "Load failed: %s\n", argv[i]);
+            ansilove_clean(&ctx);
+            continue;
+        }
+
+        opts.mode = ANSILOVE_MODE_TERMINAL;
+        if (columns > 0) {
+            opts.columns = columns;
+        }
+        
+        if (ansilove_terminal(&ctx, &opts) != 0) {
+            fprintf(stderr, "Terminal conversion failed: %s\n", argv[i]);
+            ansilove_clean(&ctx);
+            continue;
+        }
+
+        size_t output_len;
+        uint8_t *output = ansilove_terminal_emit(&ctx, &output_len);
+        
+        if (output && output_len > 0) {
+            fwrite(output, 1, output_len, stdout);
+        }
+
+        ansilove_clean(&ctx);
+    }
+    
     return 0;
 }
